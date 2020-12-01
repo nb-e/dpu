711ae4722841c874178229f77b6566b56dbb9161
nb <10951720+nb-e@users.noreply.github.com>
2020-11-30 20:50:27 -0500

    Add files via upload

---
+++ experiment/template/light_test.py
@@ -0,0 +1,57 @@
+import socket
+from socketIO_client import SocketIO, BaseNamespace
+import asyncio
+from threading import Thread
+import random
+import time
+import sys
+
+EVOLVER_IP = '192.168.1.2'
+EVOLVER_PORT = 8081
+evolver_ns = None
+socketIO = None
+
+class EvolverNamespace(BaseNamespace):
+    def on_connect(self, *args):
+        print("Connected to eVOLVER as client")
+
+    def on_disconnect(self, *args):
+        print("Discconected from eVOLVER as client")
+
+    def on_reconnect(self, *args):
+        print("Reconnected to eVOLVER as client")
+
+    def on_broadcast(self, data):
+        print(data)       
+
+def run_test(time_to_wait, selection):
+	time.sleep(time_to_wait)
+	print('Sending data...')
+	# Send temp	
+	data = {'param': 'light', 'value': [light_val] * 16, 'immediate': True}
+	print(data)
+	evolver_ns.emit('command', data, namespace = '/dpu-evolver')
+
+def start_background_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+def run_client():
+	global evolver_ns, socketIO
+	socketIO = SocketIO(EVOLVER_IP, EVOLVER_PORT)
+	evolver_ns = socketIO.define(EvolverNamespace, '/dpu-evolver')
+	socketIO.wait()
+
+if __name__ == '__main__':
+	global light_val
+	light_val = int(sys.argv[1])
+	try:
+	    new_loop = asyncio.new_event_loop()
+	    t = Thread(target = start_background_loop, args = (new_loop,))
+	    t.daemon = True
+	    t.start()
+	    new_loop.call_soon_threadsafe(run_client)
+	    time.sleep(5)
+	    run_test(0, 0)
+	except KeyboardInterrupt:
+		socketIO.disconnect()
